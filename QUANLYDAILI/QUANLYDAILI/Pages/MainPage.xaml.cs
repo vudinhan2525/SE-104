a63da55337383108c8f5ae14ae6ff319c4cf973a
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2024-04-09 18:16:21 +0700

    UI

--- QUANLYDAILI/QUANLYDAILI/Pages/MainPage.xaml.cs
+++ QUANLYDAILI/QUANLYDAILI/Pages/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using QUANLYDAILI.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections.ObjectModel;
 
 namespace QUANLYDAILI
 {
@@ -20,9 +22,49 @@ namespace QUANLYDAILI
     /// </summary>
     public partial class MainPage : Page
     {
+        private Brush originalColor;
+        
         public MainPage()
         {
             InitializeComponent();
+            originalColor = HomeBtn.Background; 
         }
+
+        private void Button_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // Thay đổi màu của nút khi focus
+            Button button = sender as Button;
+            button.Background = Brushes.Green;
+        }
+
+        // Hàm xử lý sự kiện khi nút mất focus
+        private void Button_LostFocus(object sender, RoutedEventArgs e)
+        {
+            // Đặt lại màu gốc của nút khi mất focus
+            Button button = sender as Button;
+            button.Background = originalColor;
+        }
+
+
+
+
+
+
+        private void AgentBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainMenu.Content = new AgentPage();
+        }
+
+        private void GoodsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainMenu.Content = new GoodsPage();
+            
+        }
+        
+        private void HomeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            mainMenu.Content = new MainPage();
+        }
+        
     }
 }
--- QUANLYDAILI/QUANLYDAILI/Utils/DatabaseConnector.cs
+++ QUANLYDAILI/QUANLYDAILI/Utils/DatabaseConnector.cs
@@ -6,7 +6,7 @@ namespace QUANLYDAILI.Utils
     public class DatabaseConnector
     {
         // Đổi = connection string của mỗi người
-        private readonly string cntString = @"Data Source=MSI\AN;Initial Catalog=QUANLYDAILI;Integrated Security=True";
+        private readonly string cntString = @"Data Source=DESKTOP-PEGO4F4;Initial Catalog=DaiLy;Integrated Security=True";
         public SqlConnection sqlCon = null;
 
         public void OpenConnection()
