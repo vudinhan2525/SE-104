547d34533f67f9492aaf62233b4c15ddd4567462
vudinhan2525 <vudinhan000@gmail.com>
2024-05-18 14:45:09 +0700

    upd

--- QUANLYDAILI/QUANLYDAILI/Pages/Agents/AgentPage.xaml.cs
+++ QUANLYDAILI/QUANLYDAILI/Pages/Agents/AgentPage.xaml.cs
@@ -56,6 +56,7 @@ public partial class AgentPage : Page
 
                 SqlCommand command = new SqlCommand(query, dbConnector.sqlCon);
                 SqlDataReader reader = command.ExecuteReader();
+                agents = new List<Agent>();
                 while (reader.Read())
                 {
                     Agent agent = new Agent(
@@ -73,77 +74,7 @@ public partial class AgentPage : Page
                     agents.Add(agent);
                 }
                 reader.Close();
-                for(int i = 0; i < agents.Count; i++)
-                {
-                    // Create Border element
-                    Border border = new Border();
-                    border.CornerRadius = new CornerRadius(12, 12, 0, 0);
-                    border.Margin = new Thickness(0, 20, 0, 0);
-                    border.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#cccccc"));
-                    border.BorderThickness = new Thickness(1);
-                    border.Height = 200;
-                    ImageBrush imageBrush = new ImageBrush(new BitmapImage(new Uri(agents[i].Avatar)));
-                    border.Background = imageBrush;
-                    border.Cursor = Cursors.Hand;
-                    border.Tag = i;
-                    border.MouseDown += HandleEditStore;
-                    // Create inner Border element
-                    Border innerBorder = new Border();
-                    innerBorder.CornerRadius = new CornerRadius(0, 0, 12, 12);
-                    innerBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#cccccc"));
-                    innerBorder.BorderThickness = new Thickness(1);
-                    innerBorder.Background = new SolidColorBrush(Colors.White);
-                    innerBorder.Padding = new Thickness(20, 12, 20, 12);
-                    innerBorder.Cursor = Cursors.Hand;
-                    innerBorder.Tag = i;
-                    innerBorder.MouseDown += HandleEditStore;
-                    // Create StackPanel
-                    StackPanel stackPanel = new StackPanel();
-
-                    // Add TextBlocks
-                    TextBlock textBlock1 = new TextBlock();
-                    textBlock1.Text = agents[i].TenDaiLy;
-                    textBlock1.FontSize = 20;
-                    textBlock1.FontWeight = FontWeights.Bold;
-                    stackPanel.Children.Add(textBlock1);
-
-                    TextBlock textBlock2 = new TextBlock();
-                    textBlock2.Text = agents[i].Quan + ", " + agents[i].DiaChi;
-                    stackPanel.Children.Add(textBlock2);
-
-                    TextBlock textBlock4 = new TextBlock();
-                    textBlock4.Text = "SDT: " + agents[i].SoDienThoai;
-                    textBlock4.Margin = new Thickness(0, 5, 0, 0);
-                    stackPanel.Children.Add(textBlock4);
-
-                    TextBlock textBlock3 = new TextBlock();
-                    textBlock3.Text = "Khoản nợ: " + agents[i].KhoanNo.ToString("C", CultureInfo.GetCultureInfo("vi-VN"));
-                    textBlock3.Margin = new Thickness(0, 7, 0, 4);
-                    textBlock3.FontSize = 14;
-                    textBlock3.FontWeight = FontWeights.SemiBold;
-                    stackPanel.Children.Add(textBlock3);
-
-                    // Add StackPanel to inner Border
-                    innerBorder.Child = stackPanel;
-                    
-                    if (i % 3 == 0)
-                    {
-                        stPanel1.Children.Add(border);
-                        stPanel1.Children.Add(innerBorder);
-                    }
-                    else if(i % 3 == 1)
-                    {
-                        stPanel2.Children.Add(border);
-                        stPanel2.Children.Add(innerBorder);
-                    }
-                    else if(i %3 == 2)
-                    {
-                        stPanel3.Children.Add(border);
-                        stPanel3.Children.Add(innerBorder);
-                    }
-                }
-
-
+                renderUi();
             }
             catch (Exception ex)
             {
@@ -154,6 +85,81 @@ public partial class AgentPage : Page
                 dbConnector.CloseConnection();
             }
 
+        }
+        private void renderUi()
+        {
+            stPanel1.Children.Clear();
+            stPanel2.Children.Clear();
+            stPanel3.Children.Clear();
+            for (int i = 0; i < agents.Count; i++)
+            {
+                // Create Border element
+                Border border = new Border();
+                border.CornerRadius = new CornerRadius(12, 12, 0, 0);
+                border.Margin = new Thickness(0, 20, 0, 0);
+                border.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#cccccc"));
+                border.BorderThickness = new Thickness(1);
+                border.Height = 200;
+                ImageBrush imageBrush = new ImageBrush(new BitmapImage(new Uri(agents[i].Avatar)));
+                border.Background = imageBrush;
+                border.Cursor = Cursors.Hand;
+                border.Tag = i;
+                border.MouseDown += HandleEditStore;
+                // Create inner Border element
+                Border innerBorder = new Border();
+                innerBorder.CornerRadius = new CornerRadius(0, 0, 12, 12);
+                innerBorder.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#cccccc"));
+                innerBorder.BorderThickness = new Thickness(1);
+                innerBorder.Background = new SolidColorBrush(Colors.White);
+                innerBorder.Padding = new Thickness(20, 12, 20, 12);
+                innerBorder.Cursor = Cursors.Hand;
+                innerBorder.Tag = i;
+                innerBorder.MouseDown += HandleEditStore;
+                // Create StackPanel
+                StackPanel stackPanel = new StackPanel();
+
+                // Add TextBlocks
+                TextBlock textBlock1 = new TextBlock();
+                textBlock1.Text = agents[i].TenDaiLy;
+                textBlock1.FontSize = 20;
+                textBlock1.FontWeight = FontWeights.Bold;
+                stackPanel.Children.Add(textBlock1);
+
+                TextBlock textBlock2 = new TextBlock();
+                textBlock2.Text = agents[i].Quan + ", " + agents[i].DiaChi;
+                stackPanel.Children.Add(textBlock2);
+
+                TextBlock textBlock4 = new TextBlock();
+                textBlock4.Text = "SDT: " + agents[i].SoDienThoai;
+                textBlock4.Margin = new Thickness(0, 5, 0, 0);
+                stackPanel.Children.Add(textBlock4);
+
+                TextBlock textBlock3 = new TextBlock();
+                textBlock3.Text = "Khoản nợ: " + agents[i].KhoanNo.ToString("C", CultureInfo.GetCultureInfo("vi-VN"));
+                textBlock3.Margin = new Thickness(0, 7, 0, 4);
+                textBlock3.FontSize = 14;
+                textBlock3.FontWeight = FontWeights.SemiBold;
+                stackPanel.Children.Add(textBlock3);
+
+                // Add StackPanel to inner Border
+                innerBorder.Child = stackPanel;
+
+                if (i % 3 == 0)
+                {
+                    stPanel1.Children.Add(border);
+                    stPanel1.Children.Add(innerBorder);
+                }
+                else if (i % 3 == 1)
+                {
+                    stPanel2.Children.Add(border);
+                    stPanel2.Children.Add(innerBorder);
+                }
+                else if (i % 3 == 2)
+                {
+                    stPanel3.Children.Add(border);
+                    stPanel3.Children.Add(innerBorder);
+                }
+            }
         }
         private void HandleEditStore(object sender, MouseButtonEventArgs e)
         {
@@ -169,6 +175,48 @@ public partial class AgentPage : Page
         {
             Agent a = new Agent(0, "", "", "", "", "", 0, "", 0, "");
             _menuFrame.Content = new AddAgentPage(_menuFrame, a);
+        }
+
+        private void SearchBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string query = $"SELECT * FROM DaiLy WHERE TenDaiLy LIKE @query OR Email LIKE @query";
+            try
+            {
+                dbConnector.OpenConnection();
+
+                SqlCommand command = new SqlCommand(query, dbConnector.sqlCon);
+                command.Parameters.AddWithValue("@query", "%" + SearchInp.Text.Trim() + "%");
+                SqlDataReader reader = command.ExecuteReader();
+
+                agents = new List<Agent>();
+                while (reader.Read())
+                {
+                    Agent agent = new Agent(
+                        reader.GetInt32(0),
+                        reader.GetString(1),
+                        reader.GetString(2),
+                        reader.GetString(3),
+                        reader.GetString(4),
+                        reader.GetString(5),
+                        reader.GetByte(6),
+                        reader.GetDateTime(7).ToString("yyyy-MM-dd"),
+                        reader.GetDecimal(8),
+                        reader.GetString(9)
+                    );
+                    agents.Add(agent);
+                }
+                reader.Close();
+                renderUi();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                dbConnector.CloseConnection();
+            }
+
         }
     }
 }
